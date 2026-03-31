971de22590d23f4ddf92e2abc25d252e9e8b78b2
kris-tal <mdmgiglok@gmail.com>
2026-03-31 22:02:12 +0200

    feat: add navigation bar and screen placeholders

--- app/src/main/java/com/yarnspace/app/MainActivity.kt
+++ app/src/main/java/com/yarnspace/app/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.yarnspace.app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        loadFragment(FeedFragment())
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_search -> loadFragment(SearchFragment())
+                R.id.nav_add -> loadFragment(AddFragment())
+                R.id.nav_feed -> loadFragment(FeedFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+                R.id.nav_notifs -> loadFragment(NotifsFragment())
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragment)
+            .commit()
     }
 }
